deae0db052c616ccc873136976a388160a653d23
jailovst1116 <jackilanstenstrom@gmail.com>
2025-05-29 15:34:38 +0200

    Update uppg5.js

--- uppg5.js
+++ uppg5.js
@@ -3,9 +3,13 @@ function uppg5() {
     // skriv en if-sats som jämför två tal
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
+const a = 10;
+const b = 5;
 
-
-
+if (a > b) {
+    console.log("Det första talet är större än det andra talet");
+} else if (a < b) {
+    console.log("Det andra talet är större än det första");
 }
 
 module.exports = { uppg5 };
